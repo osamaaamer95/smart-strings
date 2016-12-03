1209522844bf4274f5daa3cfe6b2b5b5c22806d4
Osama Aamer <osama.aamer95@gmail.com>
2016-12-03 13:06:04 +0500

    C++ starter code

---
+++ Header.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define TEST 4000
---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+
+int main() {
+
+	cout << "Hello World!" << endl;
+	cout << TEST << endl;
+}
